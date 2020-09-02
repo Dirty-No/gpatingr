84c6cdbe0d38df92bf9cc6769159a504f29c42e2
smaccary <smaccary@student.42.fr>
2020-09-02 22:22:39 +0300

    added sleep time arg

--- gpatingrrr.sh
+++ gpatingrrr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+SLEEP_TIME=$(echo $3 | bc)
 LINK="https://profile.intra.42.fr/users/$1"
 INTRA_PAGE=$(curl $LINK \
-    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
-    --cookie cookies.txt \
-    --compressed 2> /dev/null)
+	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
+	--cookie cookies.txt \
+	--compressed 2> /dev/null)
 
 PROJECT="$2</a>"
 
@@ -17,30 +18,35 @@ PROJECT_LINK=$(echo "$INTRA_PAGE" | grep "$PROJECT" | grep -o '".*"' | tr -d '"'
 
 while [ 1 ];do
 
-    NEW_PAGE=$(curl $PROJECT_LINK \
-        -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
-        --cookie cookies.txt \
-        --compressed 2> /dev/null)
+	NEW_PAGE=$(curl $PROJECT_LINK \
+		-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   \
+		--cookie cookies.txt \
+		--compressed 2> /dev/null)
 
-    if [ "$PROJECT_PAGE" != "$NEW_PAGE" ];
-    then
+	if [ "$PROJECT_PAGE" != "$NEW_PAGE" ];
+	then
 
-        OUTPUT=$(echo "$NEW_PAGE" | sed -n '/data-status/,$p' | sed '/project-summary-item/,$d' | sed 's/<[^<>]*>//g' | sed -r '/^\s*$/d')
-        SUCCESS=$(echo "$OUTPUT" | grep "success")
-        FAIL=$(echo "$OUTPUT" | grep "fail")
+		OUTPUT=$(echo "$NEW_PAGE" | sed -n '/data-status/,$p' | sed '/project-summary-item/,$d' | sed 's/<[^<>]*>//g' | sed -r '/^\s*$/d')
+		SUCCESS=$(echo "$OUTPUT" | grep "success")
+		FAIL=$(echo "$OUTPUT" | grep "fail")
+		GRADE=$(echo "$OUTPUT" | tail -1)
 
-        if [ "$SUCCESS" = "success" ]
-        then 
-            termux-vibrate
-            termux-tts-speak -l fr "BRAVO $1 TU AS VALIDÉ $2"
-        fi
+		if [ "$SUCCESS" = "success" ]
+		then 
+			while [ 1 ]; do
+				termux-vibrate
+				termux-tts-speak -l fr "BRAVO $1 TU A VALIDÉ $2 AVEC UNE NOTE DE $GRADE sur 100"
+			done
+		fi
 
-        if [ "$FAIL" = "fail" ]
-        then 
-            termux-vibrate
-            termux-tts-speak -l fr "BRAVO $1 TU AS RATÉ $2"
-        fi
+		if [ "$FAIL" = "fail" ]
+		then 
+			while [ 1 ]; do
+				termux-vibrate
+				termux-tts-speak -l fr "BRAVO $1 TU A RATÉ $2 AVEC UNE NOTE DE $GRADE sur 100"
+			done
+		fi
 
-    fi
-
+	fi
+	sleep $SLEEP_TIME
 done
